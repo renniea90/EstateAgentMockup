f18e614e08fef2017648435932d279ead3aacaed
SuzyQ <153325350+SuzyQ-2117@users.noreply.github.com>
2024-05-31 11:37:15 +0100

    merged files

--- estate-agent-website/src/App.js
+++ estate-agent-website/src/App.js
@@ -2,10 +2,25 @@ import AllBuyers from './components/AllBuyers';
 import AllSellers from './components/AllSellers';
 import Data from './data/Data.json';
 import './App.css';
+import DisplayProperty from './components/DisplayProperty';
+import RegisterBuyer from './components/RegisterBuyer';
+import Navbar from './components/Navbar';
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import HomePage from './pages/Home';
+import BuyersPage from './pages/Buyers';
+import SellersPage from './pages/Sellers';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/buyers' element={<BuyersPage />} />
+            <Route path='/sellers' element={<SellersPage />} />
+          </Routes>
+        </BrowserRouter>
       <main>
           <AllBuyers />
           <AllSellers />
