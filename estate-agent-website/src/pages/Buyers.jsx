f37a78cf2cbd0677bbb27b6141cf4a2c58e0176f
SuzyQ-2117 <153325350+SuzyQ-2117@users.noreply.github.com>
2024-05-31 12:46:43 +0100

    components rearranged into relevant pages, fixed .map issue with buyers and sellers by adding .buyer or .seller to map function

--- estate-agent-website/src/pages/Buyers.jsx
+++ estate-agent-website/src/pages/Buyers.jsx
@@ -1,7 +1,21 @@
+import Data from '../data/Data.json'
+import AllBuyers from '../components/AllBuyers'
+import RegisterBuyer from '../components/RegisterBuyer'
+
 export default function BuyersPage() {
     return (
         <div>
             BUYERS PAGE
+            <div className="customer-grid">
+              {Data.Buyers.map((item) => (
+                <AllBuyers
+                ID={item.ID}
+                FirstName={item.FirstName}
+                SurName={item.SurName}
+                />
+              ))}
+            </div>
+            <RegisterBuyer />
         </div>
     )
 }
--- estate-agent-website/src/pages/Home.jsx
+++ estate-agent-website/src/pages/Home.jsx
@@ -1,7 +1,10 @@
+import DisplayProperty from "../components/DisplayProperty"
+
 export default function HomePage() {
     return (
         <div>
             HOME PAGE
+            <DisplayProperty />
         </div>
     )
 }
