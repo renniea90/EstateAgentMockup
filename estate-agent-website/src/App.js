a2b4a4bcded2998923ee6f4d9de56d7fce62d3f6
renniea90 <rennie.a90@gmail.com>
2024-05-31 11:31:55 +0100

    Branch3 push

--- estate-agent-website/src/App.js
+++ estate-agent-website/src/App.js
@@ -1,23 +1,29 @@
-import DisplayProperty from './components/DisplayProperty';
+import AllBuyers from './components/AllBuyers';
+import AllSellers from './components/AllSellers';
+import Data from './data/Data.json';
 import './App.css';
-import RegisterBuyer from './components/RegisterBuyer';
-import Navbar from './components/Navbar';
-import { Routes, Route, BrowserRouter } from 'react-router-dom'
-import HomePage from './pages/Home';
-import BuyersPage from './pages/Buyers';
-import SellersPage from './pages/Sellers';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/buyers' element={<BuyersPage />} />
-          <Route path='/sellers' element={<SellersPage />} />
-        </Routes>
-      </BrowserRouter>
+      <main>
+          <AllBuyers />
+          <AllSellers />
+            <div className="customer-grid">
+              {Data.map((item) => (
+                <AllBuyers
+                ID={item.ID}
+                FirstName={item.FirstName}
+                SurName={item.SurName}
+                />,
+                <AllSellers
+                ID={item.ID}
+                FirstName={item.FirstName}
+                SurName={item.SurName}
+                />
+              ))}
+            </div>
+      </main>
     </div>
   );
 }
---
+++ estate-agent-website/src/components/AllSellers.jsx
@@ -0,0 +1,20 @@
+
+function AllSellers({ ID, FirstName, SurName}) { 
+
+    return ( 
+
+        <div className="all-sellers"> 
+
+            <p>{ID}</p> 
+
+            <p>{FirstName}</p> 
+
+            <p>{SurName}</p> 
+
+        </div> 
+
+    ) 
+
+} 
+
+export default AllSellers
