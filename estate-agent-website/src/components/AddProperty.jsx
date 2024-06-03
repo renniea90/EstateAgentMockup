164a0d753887870eab6074593478531aebde7153
Lornags <lornagordonsmith@gmail.com>
2024-06-03 17:21:12 +0100

    Added new drop down to display seller firstname & surname  data from data.json file Added functionality that if the seller exists then add new property to the json file, and if not give an alert to register as a seller on the seller page Added functionality to default Sale Status to FORSALE when adding new property

--- estate-agent-website/src/components/AddProperty.jsx
+++ estate-agent-website/src/components/AddProperty.jsx
@@ -1,130 +1,160 @@
-import {useState} from 'react'
-import '../CSS/AddProperty.css'
+import { useState } from "react";
+import "../CSS/AddProperty.css";
+import { SiThenorthface } from "react-icons/si";
+import Data from "../data/Data.json";
+
+export default function AddProperty() {
+  // create state
+
+  const [ImageUrl, setImageUrl] = useState("");
+  const [Address, setAddress] = useState("");
+  const [Price, setPrice] = useState(0);
+  const [Bedrooms, setBedrooms] = useState(1);
+  const [Bathrooms, setBathrooms] = useState(1);
+  const [Garden, setGarden] = useState("No");
+  const [SaleStatus, setSaleStatus] = useState("FORSALE");
+  const [Seller, setSeller] = useState("")
 
 
+  const handleSubmit = (e) => {
+    // tells the event if the event doesnt get handled dont use the default action as I want to do something else
+    e.preventDefault();
 
-export default function AddProperty () {
+    
+// If seller exists then post to JSON
+if (Seller != ""){
+       
 
-    // create state
+                    const task = {
+                    ImageUrl,
+                    Address,
+                    Price,
+                    Bedrooms,
+                    Bathrooms,
+                    Garden,
+                    SaleStatus,
+                    };
 
-    const [ImageUrl, setImageUrl] = useState(''); 
-    const [Address, setAddress] = useState(''); 
-    const [Price, setPrice] = useState(0); 
-    const [Bedrooms, setBedrooms] = useState(1); 
-    const [Bathrooms, setBathrooms] = useState(1); 
-    const [Garden, setGarden] = useState('No'); 
-    const [SaleStatus, setSaleStatus] = useState('FORSALE'); 
+                    //code to be added for if new then set to forsale
+                    setSaleStatus("FORSALE");
 
+                    // const apiURL = {http://localhost:8000/animals'}
+                    fetch(
+                    "http://localhost:8000/Properties",
 
+                    {
+                        method: "POST",
+                        // for most api json call
+                        headers: { "Content-Type": "application/json" },
+                        // changing into json data
+                        body: JSON.stringify(task),
+                    }
+                    );
+                  
 
+                    alert("New Property Added");
 
-    const handleSubmit = (e) => {
+              
 
-        // tells the event if the event doesnt get handled dont use the default action as I want to do something else
-        e.preventDefault();
-        const task = {ImageUrl, Address, Price, Bedrooms, Bathrooms, Garden, SaleStatus}
-        // const apiURL = {http://localhost:8000/animals'}
-        fetch('http://localhost:8000/Properties',
-        
-        {
-                method: 'POST',
-                // for most api json call
-                headers: {"Content-Type" : "application/json"},
-                // changing into json data
-                body: JSON.stringify(task)
-        }
-   
-    )
+    // reset text boxes
 
-        alert("New Property Added")
+    setImageUrl("");
+    setAddress("");
+    setPrice(0);
+    setBedrooms(0);
+    setBathrooms(0);
+    setGarden("");
+    setSaleStatus("");
+    setSeller("");
 
-        // reset text boxes
-        setImageUrl('')
-        setAddress('')
-        setPrice(0)
-        setBedrooms(0)
-        setBathrooms(0)
-        setGarden('')
-        setSaleStatus('')
+    //otherwise ask seller to register
+} else {
 
-    }
+    alert("Seller doesnt exist, please register in the seller page");
 
-    return(
+} 
 
+  };
 
-
-        <form onSubmit={handleSubmit}>
-              <label for="fname"> Address :</label>
-                    <input id="fname" type='text'
-                    required
-                    value={Address}
-                    // event
-                    onChange={(e) => setAddress(e.target.value)} />
-
-            <br />   <br />   <br />
-
-            <label><span>Price  :</span> </label>
-            <input type='Number'
-            required
-            value={Price}
-            // event is on change ,  target value is whats in the input box
-            onChange={(e) => setPrice(e.target.value)} />
-
-            <br />   <br />   <br />
-
-            <label>Image URL : </label>
-            <input type='text'
-            required
-            value={ImageUrl}
-            // event
-            onChange={(e) =>setImageUrl(e.target.value)} />
-
-            <br />   <br />   <br />
-
-            <label>Bedrooms : </label>
-            <input type='number'
-            required
-            value={Bedrooms}
-            // event
-            onChange={(e) =>setBedrooms(e.target.value)} />
-
-            <br />   <br />   <br />
-
-            <label>Bathrooms : </label>
-            <input type='number'
-            required
-            value={Bathrooms}
-            // event
-            onChange={(e) =>setBathrooms(e.target.value)} />
-
-            <br />   <br />   <br />
-
-
-            <label>Garden : </label>
-            <select 
-                    value={Garden}
-            // event
-            onChange={(e) =>setGarden(e.target.value)} >
-                    <option value="Yes">Yes</option>
-                    <option value="No">No</option>
-            </select>
-            <br />   <br />   <br />
-
-            <label>Sale Status : </label>
-            <select 
-                    value={SaleStatus}
-            // event
-            onChange={(e) =>setSaleStatus(e.target.value)} >
-                 <option value="FORSALE">FORSALE</option>
-                 <option value="WITHDRAWN">WITHDRAWN</option>
-                 <option value="SOLD">SOLD</option>
-            </select>
-            <br />   <br />   <br />
-            <button>Add Property</button>
-            <br />   <br />   <br />
-        </form>
-
-    )
-
-
+  return (
+    <form onSubmit={handleSubmit}>
+      {/* <label>Existing Seller : </label>
+      <select onChange={(e) => CheckifExisitngSeller(e.target.value === "Yes")}>
+        <option value=""></option>
+        <option value="Yes">Yes</option>
+        <option value="No">No</option>
+      </select> */}
+      {/* <br /> <br /> <br /> */}
+      <label>Existing Sellers : </label>
+      <select name="Sellers"
+            onChange={(e) => setSeller(e.target.value)}
+            >
+           <option value=""></option>
+        {Data.Sellers.map((item) => (
+          <option value={item.ID}>{item.FirstName + " " + item.SurName}</option>
+        ))}
+    </select>
+      <br /> <br /> <br />
+      <label for="fname"> Address :</label>
+      <input
+        id="fname"
+        type="text"
+        required
+        value={Address}
+        // event
+        onChange={(e) => setAddress(e.target.value)}
+      />
+      <br /> <br /> <br />
+      <label>
+        <span>Price :</span>{" "}
+      </label>
+      <input
+        type="Number"
+        required
+        value={Price}
+        // event is on change ,  target value is whats in the input box
+        onChange={(e) => setPrice(e.target.value)}
+      />
+      <br /> <br /> <br />
+      <label>Image URL : </label>
+      <input
+        type="text"
+        required
+        value={ImageUrl}
+        // event
+        onChange={(e) => setImageUrl(e.target.value)}
+      />
+      <br /> <br /> <br />
+      <label>Bedrooms : </label>
+      <input
+        type="number"
+        required
+        value={Bedrooms}
+        // event
+        onChange={(e) => setBedrooms(e.target.value)}
+      />
+      <br /> <br /> <br />
+      <label>Bathrooms : </label>
+      <input
+        type="number"
+        required
+        value={Bathrooms}
+        // event
+        onChange={(e) => setBathrooms(e.target.value)}
+      />
+      <br /> <br /> <br />
+      <label>Garden : </label>
+      <select
+        value={Garden}
+        // event
+        onChange={(e) => setGarden(e.target.value)}
+      >
+        <option value="Yes">Yes</option>
+        <option value="No">No</option>
+      </select>
+      <br /> <br /> <br />
+      <button>Add Property</button>
+      <br /> <br /> <br />
+    </form>
+  );
 }
